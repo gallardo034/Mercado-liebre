9184d748944dc8ca5e2f27fc1850a11bace56447
gallardo034 <gallardoagustinm@gmail.com>
2023-07-13 22:28:09 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const path = require ('path');
+const dotenv = require('dotenv').config();
+
+
+const app = express();
+
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use (express.static(publicPath));
+
+
+app.get ('/palabras'), (req, res) => {
+    res.send ('palabras random');
+};
+
+app.get ('/', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './views/home.html'))
+});
+
+app.get ('/register', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './views/register.html'))
+});
+
+app.get ('/login', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './views/login.html'))
+});
+
+
+app.listen (process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto 3000' + process.env.PORT + '- http://localhost:3000');
+});
+
